0f012173c7700d04d53c7bdff9ca791767a4120f
taoista <taoista-@hotmail.com>
2026-05-24 11:16:29 -0400

    fix correccion

--- lib/screens/food_screen/header_foods.dart
+++ lib/screens/food_screen/header_foods.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:recetastao/models/foods.dart';
 import 'package:recetastao/models/recipe.dart';
 
-
 class HeaderFoods extends StatelessWidget {
-
   final Recipe recipe;
 
   const HeaderFoods({super.key, required this.recipe});
@@ -12,47 +9,46 @@ class HeaderFoods extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Stack(
+      borderRadius: BorderRadius.circular(12),
+      child: Stack(
+        children: [
+          Image.asset(
+            recipe.imageUrl,
+            height: 200,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            top: 10,
+            right: 10,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              decoration: BoxDecoration(
+                color: Colors.white70,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.star, color: Colors.orange, size: 16),
+                  SizedBox(width: 4),
+                  Text("${recipe.top}"),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 10,
+            left: 10,
+            child: Row(
               children: [
-                Image.asset(
-                  recipe.imageUrl,
-                  height: 200,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-                Positioned(
-                  top: 10,
-                  right: 10,
-                  child: Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                    decoration: BoxDecoration(
-                      color: Colors.white70,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Row(
-                      children: [
-                        Icon(Icons.star, color: Colors.orange, size: 16),
-                        SizedBox(width: 4),
-                        Text("${recipe.top}"),
-                      ],
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 10,
-                  left: 10,
-                  child: Row(
-                    children: [
-                      Icon(Icons.access_time, color: Colors.white),
-                      SizedBox(width: 5),
-                      Text("${recipe.time}", style: TextStyle(color: Colors.white)),
-                    ],
-                  ),
-                )
+                Icon(Icons.access_time, color: Colors.white),
+                SizedBox(width: 5),
+                Text("${recipe.time}", style: TextStyle(color: Colors.white)),
               ],
             ),
-          );
+          )
+        ],
+      ),
+    );
   }
 }
